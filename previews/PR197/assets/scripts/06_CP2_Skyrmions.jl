1d5287a5f654f63fbb9bdb064bc18da642eefd6a
Documenter.jl <documenter@juliadocs.github.io>
2023-11-25 21:49:11 +0000

    build based on 88af6cb

--- previews/PR197/assets/scripts/06_CP2_Skyrmions.jl
+++ previews/PR197/assets/scripts/06_CP2_Skyrmions.jl
@@ -12,17 +12,17 @@ J1 = -1           # Nearest-neighbor ferromagnetic
 J2 = (2.0/(1+√5)) # Tune competing exchange to set skyrmion scale length
 Δ = 2.6           # Exchange anisotropy
 
-ex1 = J1 * [1.0 0.0 0.0;
-            0.0 1.0 0.0;
-            0.0 0.0 Δ]
-ex2 = J2 * [1.0 0.0 0.0;
-            0.0 1.0 0.0;
-            0.0 0.0 Δ]
+ex1 = J1 * [1 0 0;
+            0 1 0;
+            0 0 Δ]
+ex2 = J2 * [1 0 0;
+            0 1 0;
+            0 0 Δ]
 set_exchange!(sys, ex1, Bond(1, 1, [1, 0, 0]))
 set_exchange!(sys, ex2, Bond(1, 1, [1, 2, 0]))
 
 h = 15.5
-field = set_external_field!(sys, [0.0 0.0 h])
+field = set_external_field!(sys, [0, 0, h])
 
 D = 19.0
 set_onsite_coupling!(sys, S -> D*S[3]^2, 1)
@@ -37,7 +37,7 @@ kT = 0      # Target equilibrium temperature (meV)
 λ = 0.1     # Magnitude of coupling to thermal bath (dimensionless)
 integrator = Langevin(Δt; kT, λ)
 
-τs = [4., 16, 256]  # Times to record snapshots
+τs = [4, 16, 256]   # Times to record snapshots
 frames = []         # Empty array to store snapshots
 for i in eachindex(τs)
     dur = i == 1 ? τs[1] : τs[i] - τs[i-1] # Determine the length of time to simulate
@@ -59,5 +59,5 @@ function sun_berry_curvature(z₁, z₂, z₃)
 end
 
 plot_triangular_plaquettes(sun_berry_curvature, frames; size=(600,200),
-    offset_spacing=10, texts = ["\tt = "*string(τ) for τ in τs], text_offset = (0.0, 6.0)
+    offset_spacing=10, texts=["\tt = "*string(τ) for τ in τs], text_offset=(0, 6)
 )
